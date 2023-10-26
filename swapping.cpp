fcf67422f7cd7b147c9ae0e706322c10820705b7
jeevansh051 <140502783+jeevansh051@users.noreply.github.com>
2023-10-27 00:13:08 +0530

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bitset>
+
+using namespace std;
+
+
+int main()
+{
+int x,y;
+cout<<"enter first number: ";
+cin>>x;
+cout<<"enter second number: ";
+cin>>y;
+cout<<"first number is:  "<<x<<endl;
+cout<<"second number is: "<<y<<endl;
+swap(x,y);
+cout<<"first number is:  "<<x<<endl;
+cout<<"second number is: "<<y;
+
+ 
+}
